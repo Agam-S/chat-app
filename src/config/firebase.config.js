53dfd050630fd5f4f236d73598b9f417a39022a0
Agam-S <singh.agam0009@gmail.com>
2024-02-23 21:36:38 +1100

    first

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -6,6 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
