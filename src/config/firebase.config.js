eb1490655b957bf7e8cbbec05071ae6de1d6e8de
Agam-S <singh.agam0009@gmail.com>
2024-02-23 19:12:37 +1100

    first commit

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -6,15 +6,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC6nSKFAnLm6L4ayb1TMLBnbVbHcwbckm4",
-  authDomain: "chat-app-2699c.firebaseapp.com",
-  projectId: "chat-app-2699c",
-  storageBucket: "chat-app-2699c.appspot.com",
-  messagingSenderId: "789820638523",
-  appId: "1:789820638523:web:5b069ad225374dfbace31e",
-  measurementId: "G-7WVE9GMW4E",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
