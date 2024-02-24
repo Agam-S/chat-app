b105ac18f88172add771cb8c62d2e10fd1571933
Agam-S <singh.agam0009@gmail.com>
2024-02-24 19:31:39 +1100

    started custom button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import CustomStyles from "../theme/CustomStyles";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <Button style={CustomStyles.pinkButton}>Hi</Button>
+      <h1>Home Page</h1>
+    </>
+  );
 };
 
 export default Home;
