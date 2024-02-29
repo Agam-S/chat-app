15effe2d90211c2175cedce9cb69e45e6433d8f2
Agam-S <singh.agam0009@gmail.com>
2024-02-29 18:50:28 +1100

    done with nav bar dynamics?

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Nav.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { signOut, getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setShowLogin(user !== null);
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Logged Out");
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <>
       <h1 className="nav-title">Private Chat App</h1>
@@ -37,9 +60,20 @@ const Nav = () => {
           <li>
             <Link to="/Contact">Contact Us</Link>
           </li>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
+          {showLogin ? (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+          {showLogin && (
+            <li>
+              <Link onClick={logout}>Logout</Link>
+            </li>
+          )}
         </ul>
       </nav>
     </>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,15 +4,32 @@ import {
   createUserWithEmailAndPassword,
   signInWithPopup,
   signInWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
 } from "firebase/auth";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { auth, provider } from "../config/firebase.config";
 import PinkButton from "../theme/PinkButton";
 import { Button } from "react-bootstrap";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/");
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   const [showSignup, setShowSignup] = useState(false);
 
+  const [userName, setuserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -34,9 +51,23 @@ const Login = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const auth = getAuth();
-      const user = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(user);
+      if (userName.length === 0) {
+        alert("name cannot be empty");
+      } else if (email.length === 0 || password.length === 0) {
+        alert("email or password cannot be empty");
+      } else {
+        const auth = getAuth();
+        const { user } = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        await updateProfile(user, {
+          displayName: userName,
+        });
+        setError(`Account created for ${userName}`);
+        console.log(user);
+      }
     } catch (error) {
       setError(error.message);
     }
@@ -54,6 +85,8 @@ const Login = () => {
         passwordIn
       );
       console.log(userIn);
+      setError(`Logged In As ${userIn.user.displayName}`);
+      navigate("/chat");
     } catch (error) {
       setError(error.message);
     }
@@ -61,19 +94,26 @@ const Login = () => {
   };
 
   const signInWithGoogle = async () => {
-    const result = await signInWithPopup(auth, provider);
-    console.log(result);
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log("Google Sign-In Successful", result);
+      navigate("/chat");
+    } catch (error) {
+      console.error("Google Sign-In Failed", error.message);
+    }
   };
 
   return (
-    <div>
+    <div className="login-page">
       <PinkButton />
-      <Button variant="pink" onClick={handleSignupClick}>
-        Sign Up
-      </Button>
-      <Button variant="pink" onClick={handleLoginClick}>
-        Login
-      </Button>
+      <div className="button-nav">
+        <Button variant="outline-dark" onClick={handleSignupClick}>
+          Sign Up
+        </Button>
+        <Button variant="outline-dark" onClick={handleLoginClick}>
+          Login
+        </Button>
+      </div>
       <h1>{loading ? "Loading..." : ""}</h1>
       {showSignup ? (
         <div className="signup-form">
@@ -84,6 +124,12 @@ const Login = () => {
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              value={userName}
+              onChange={(e) => setuserName(e.target.value)}
             />
             <input
               type="password"
